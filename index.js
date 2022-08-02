b62765b60de71bd0d430295f4db62655d7dfc5f1
lily wang <benitawang@tamu.edu>
2022-08-02 17:23:09 -0500

    pushing carter's code for him because his computer is not working very well right now; updated code for the order crud

--- index.js
+++ index.js
@@ -10,73 +10,83 @@ app.use(express.json());
 // ROUTES
 
 //create a todo THIS IS AN EXAMPLE FOR YALL TO GET STARTED
-app.post("/todos", async (reque, respon) => {
+app.post("/OrderDML", async (reque, respon) => {
 	try {
 		// console.log(reque.body);
-		const { description } = reque.body;
+		const { deliveryStatus, orderDate, quantity, coid, cid } = reque.body;
 		const newTodo = await pool.query(
-			"INSERT INTO todo (description) VALUES($1) RETURNING *",
-			[description]
+			`INSERT INTO "OCR"."Order" (deliveryStatus, orderDate, quantity, coid, cid) VALUES($1, $2, $3, $4, $5)`,
+			[deliveryStatus, orderDate, quantity, coid, cid]
 		);
-		respon.json(newTodo.rows[0]);
+		respon.json({
+			message: "New Order created",
+			body: {
+				order: { deliveryStatus, orderDate, quantity, coid, cid }
+			}
+		});
 	} catch (err) {
+		respon.json(err);
 		console.error(err.message);
 	}
 });
-//get all todos
-app.get("/todos", async (reque, respon) => {
+
+//get all Orders
+app.get("/OrderDML", async (reque, respon) => {
 	try {
-		const allTodos = await pool.query("SELECT * FROM todo;");
-		respon.json(allTodos.rows);
+		const allOrders = await pool.query(`SELECT * FROM "OCR"."Order";`);
+		respon.json(allOrders.rows);
 	} catch (err) {
+		respon.json(err);
 		console.error(err.message);
 	}
 });
-//get a todo
-app.get("/todos/:id", async (reque, respon) => {
+
+//get an Order by id
+app.get("/OrderDML/:id", async (reque, respon) => {
 	try {
 		const { id } = reque.params;
-		const todo = await pool.query(
-			"SELECT description FROM todo WHERE todo_id = $1",
+		const Order = await pool.query(
+			`SELECT * FROM "OCR"."Order" WHERE oid = $1`,
 			[id]
 		);
-		respon.json(todo.rows[0]);
+		respon.json(Order.rows[0]);
 	} catch (err) {
+		respon.json(err);
 		console.error(err.message);
 	}
 });
 
-//update a todo
-app.put("/todos/:id", async (reque, respon) => {
+//update an Order
+app.put("/OrderDML/:id", async (reque, respon) => {
 	try {
-		const { id } = reque.params;
-		const { description } = reque.body;
+		const id = parseInt(req.params.id);
+		const { deliveryStatus, orderDate, quantity, coid, cid } = reque.body;
 
 		const updateTodo = await pool.query(
-			"UPDATE todo SET description = $1 WHERE todo_id = $2",
-			[description, id]
+			`UPDATE "OCR"."Order" SET deliveryStatus = $1, orderDate = $2, quantity = $3, coid = $4, cid = $5 WHERE oid = $6`,
+			[ deliveryStatus, orderDate, quantity, coid, cid, id]
 		);
-		respon.json("todo was updated");
+		respon.json("Order was updated");
 	} catch (err) {
+		respon.json(err);
 		console.error(err.message);
 	}
 });
-//delete  a todo
-app.delete("/todos/:id", async (reque, respon) => {
+
+//delete an Order
+app.delete("/OrderDML/:id", async (reque, respon) => {
 	try {
 		const { id } = reque.params;
-		const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+		const deleteTodo = await pool.query(`DELETE FROM "OCR"."Order" WHERE oid = $1`, [
 			id,
 		]);
 		respon.json("todo was deleted");
 	} catch (err) {
+		respon.json(err);
 		console.error(err.message);
 	}
 });
-// END EXAMPLE
-//
-//
-//
+
 app.listen(3001, () => {
 	console.log("server has started on port 3001");
 });
